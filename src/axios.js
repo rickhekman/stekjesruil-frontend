9865a638474e937e8eb65cca1a4e0963937697c0
rickhekman <rickhekman@gmail.com>
2020-04-24 11:02:50 +0200

    Fix Axios instance for deployment

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000'
+  // baseURL: 'http://localhost:4000'
+  baseURL: 'https://stekjesruil.herokuapp.com/'
 });
 
 export default instance

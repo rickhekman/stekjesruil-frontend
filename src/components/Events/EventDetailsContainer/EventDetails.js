a8cae9bb6b8616041744e5c46fac044bba84e663
rickhekman <rickhekman@gmail.com>
2020-06-17 16:38:22 +0200

    Fix create events, cuttings form containers state
    EmptyForm const defenition to make container state more cleaner

--- src/components/Events/EventDetailsContainer/EventDetails.js
+++ src/components/Events/EventDetailsContainer/EventDetails.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { Spring } from 'react-spring/renderprops';
 import Moment from 'react-moment';
 
 import TrashIcon from '../../../img/icons/trashcan.svg';
@@ -17,69 +16,58 @@ function EventDetails(props) {
     <div>
       {event
         ? <div className="eventpopup">
-          <Spring
-            from={{ opacity: 0 }}
-            to={{ opacity: 1 }}
-          >
-            {props => (
-              <div style={props}>
 
-                <div className="eventdetails">
+          <div className="eventdetails">
 
 
-                  <Link to={`/events/`}
-                    style={{
-                      textDecoration: 'none'
-                    }}
-                    key={event}
-                    className="eventdetails__close"
-                  >
-                    <p>X</p>
-                  </Link>
+            <Link to={`/events/`}
+              style={{
+                textDecoration: 'none'
+              }}
+              key={event}
+              className="eventdetails__close"
+            >
+              <p>X</p>
+            </Link>
 
-                  <img src={event.photo} alt={event.title} className="eventdetails__photo" />
-                  <h1 className="eventdetails__title heading--primary">{event.title}</h1>
-                  <div className="eventdetails__date">
-                    <p>
-                      <Moment format="dddd D MMMM YYYY">
-                        {event.startdate}
-                      </Moment>
-                    </p>
-                  </div>
-                  <div className="eventdetails__time">
-                    <p>
-                      <Moment format="HH:mm -">
-                        {event.startdate}
-                      </Moment>
-                      <Moment format=" HH:mm">
-                        {event.enddate}
-                      </Moment>
-                    </p>
-                  </div>
-                  <div className="eventdetails__description">
-                    <p className="paragraph">{event.description}</p>
-                  </div>
-                  <div className="eventdetails__location">
-                    <p className="paragraph" style={{ fontWeight: '600', fontSize: '1.5rem' }}>{t('event.location')}</p>
-                    <p className="paragraph">{event.locationname}</p>
-                    <p className="paragraph">{event.address1}</p>
-                    <p className="paragraph">{event.address2}</p>
-                    <p className="paragraph">{event.zipcode} {event.city}</p>
-                    <p className="paragraph">{event.country}</p>
-                    <p className="paragraph">{event.geolocation}</p>
-                    <button type="button" onClick={props.onWebsiteClick} className="eventdetails__btn-website btn btn--yellow">{t('event.website')}</button>
-                  </div>
-                  <div className="eventdetails__fb-event" onClick={props.onClick}>
-                    <img src={FbIcon} alt="icon" /><p className="paragraph">{t('event.fb')}</p>
-                  </div>
-                  {/* <img src={TrashIcon} type="button" onClick={props.onDelete} className="eventdetails__delete" alt="delete icon" /> */}
-                  <img src={TrashIcon} type="button" onClick={props.onDelete} className="eventdetails__delete" alt="delete icon" />
-                </div>
+            <img src={event.photo} alt={event.title} className="eventdetails__photo" />
+            <h1 className="eventdetails__title heading--primary">{event.title}</h1>
+            <div className="eventdetails__date">
+              <p>
+                <Moment format="dddd D MMMM YYYY">
+                  {event.startdate}
+                </Moment>
+              </p>
+            </div>
+            <div className="eventdetails__time">
+              <p>
+                <Moment format="HH:mm -">
+                  {event.startdate}
+                </Moment>
+                <Moment format=" HH:mm">
+                  {event.enddate}
+                </Moment>
+              </p>
+            </div>
+            <div className="eventdetails__description">
+              <p className="paragraph">{event.description}</p>
+            </div>
+            <div className="eventdetails__location">
+              <p className="paragraph" style={{ fontWeight: '600', fontSize: '1.5rem' }}>{t('event.location')}</p>
+              <p className="paragraph">{event.locationname}</p>
+              <p className="paragraph">{event.address1}</p>
+              <p className="paragraph">{event.address2}</p>
+              <p className="paragraph">{event.zipcode} {event.city}</p>
+              <p className="paragraph">{event.country}</p>
+              <p className="paragraph">{event.geolocation}</p>
+              <button type="button" onClick={props.onWebsiteClick} className="eventdetails__btn-website btn btn--yellow">{t('event.website')}</button>
+            </div>
+            <div className="eventdetails__fb-event" onClick={props.onClick}>
+              <img src={FbIcon} alt="icon" /><p className="paragraph">{t('event.fb')}</p>
+            </div>
 
-
-              </div>
-            )}
-          </Spring>
+            <img src={TrashIcon} type="button" onClick={props.onDelete} className="eventdetails__delete" alt="delete icon" />
+          </div>
 
         </div>
         : 'Event details are loading...'

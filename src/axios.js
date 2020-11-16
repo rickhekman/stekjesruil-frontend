e53714c0ba0732264523ad82171564eae99c876c
rickhekman <rickhekman@gmail.com>
2020-11-16 12:33:22 +0100

    Fix restore heroku url axios create

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-if (process.env.DATABASE_URL) {
-Axios.defaults.baseURL = process.env.DATABASE_URL;
-}
-
 const instance = axios.create({
   // baseURL: 'http://localhost:4000'
   baseURL: 'https://stekjesruil.herokuapp.com/' 

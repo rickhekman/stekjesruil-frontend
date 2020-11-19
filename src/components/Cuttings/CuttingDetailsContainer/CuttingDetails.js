50c313d0072e5d62f02804b5ee25b44aa1435a10
rickhekman <rickhekman@gmail.com>
2020-11-19 16:33:05 +0100

    Hide elements

--- src/components/Cuttings/CuttingDetailsContainer/CuttingDetails.js
+++ src/components/Cuttings/CuttingDetailsContainer/CuttingDetails.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 
 function CuttingDetails(props) {
 
   const { cutting } = props
 
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
 
   return (
     <div>
@@ -14,8 +14,7 @@ function CuttingDetails(props) {
           <h1 className="cutting-details__title">{cutting.name}</h1>
           <img src={cutting.photo} alt={cutting.name} className="cutting-details__photo" />
           <p className="cutting-details__description">{cutting.description}</p>
-
-          <button type="button" onClick={props.onDelete} className="cutting-details__delete-button">{t('cutting.delete')}</button>
+          {/* <button type="button" onClick={props.onDelete} className="cutting-details__delete-button">{t('cutting.delete')}</button> */}
         </div>
         : 'Cutting details are loading...'
       }
